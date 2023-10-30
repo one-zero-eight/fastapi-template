d11ffee49db2cf8b2f31bf9ada836e51cb7eecdf
Ruslan Bel'kov <ruslan.belckov@yandex.ru>
2023-10-30 15:38:36 +0300

    chore: formatting

--- src/schemas/__init__.py
+++ src/schemas/__init__.py
@@ -1,6 +1,12 @@
 # fmt: off
 __all__ = [
-
+    "CreateSomeSchemeInSingle", "ViewSomeSchemeInSingle", "UpdateSomeSchemeInSingle",
 ]
+
 # fmt: on
 
+from src.schemas.some_scheme_in_plural import (
+    CreateSomeSchemeInSingle,
+    ViewSomeSchemeInSingle,
+    UpdateSomeSchemeInSingle,
+)
--- src/schemas/some_scheme_in_plural.py
+++ src/schemas/some_scheme_in_plural.py
@@ -1,3 +1,9 @@
+__all__ = [
+    "CreateSomeSchemeInSingle",
+    "ViewSomeSchemeInSingle",
+    "UpdateSomeSchemeInSingle",
+]
+
 from pydantic import BaseModel
 
 
@@ -11,6 +17,7 @@ class ViewSomeSchemeInSingle(BaseModel):
 
 class UpdateSomeSchemeInSingle(BaseModel):
     ...
+
 
 # Note: if some relation is needed, add it here
 # from src.schemas.some_scheme2_in_plural import ViewSomeScheme2InPlural
