d7a80cf8acd0db44a5f4c545ceae8aa2e094d169
Ruslan Bel'kov <ruslan.belckov@yandex.ru>
2023-11-29 19:09:43 +0300

    chore: setup predefined; get engine from settings method

--- src/api/startup.py
+++ src/api/startup.py
@@ -1,18 +1,23 @@
-import warnings
+__all__ = [
+    "setup_repositories",
+    "setup_admin_panel",
+    "setup_predefined",
+]
 
 from fastapi import FastAPI
 
-from src.config import settings, Environment
+from src.config import settings
+from src.config_schema import Environment
 from src.modules.auth.repository import AuthRepository
+from src.api.dependencies import Dependencies
 
 
 async def setup_repositories():
     from src.modules.users.repository import UserRepository
     from src.storages.sqlalchemy.storage import SQLAlchemyStorage
-    from src.api.dependencies import Dependencies
 
     # ------------------- Repositories Dependencies -------------------
-    storage = SQLAlchemyStorage.from_url(settings.DB_URL.get_secret_value())
+    storage = SQLAlchemyStorage(settings.DATABASE.get_async_engine())
     user_repository = UserRepository(storage)
     auth_repository = AuthRepository(storage)
 
@@ -20,20 +25,23 @@ async def setup_repositories():
     Dependencies.set_storage(storage)
     Dependencies.set_user_repository(user_repository)
 
-    # await storage.create_all()
-
     if settings.ENVIRONMENT == Environment.DEVELOPMENT:
         import logging
 
-        logging.basicConfig()
         logging.getLogger("sqlalchemy.engine").setLevel(logging.WARNING)
-        warnings.warn("SQLAlchemy logging is enabled!")
+        logging.info("SQLAlchemy logging is enabled!")
 
 
-def setup_admin(app: FastAPI):
+def setup_admin_panel(app: FastAPI):
     from src.modules.admin.app import init_app
-    from sqlalchemy.ext.asyncio import create_async_engine
 
-    engine = create_async_engine(settings.DB_URL.get_secret_value(), pool_recycle=3600)
+    init_app(app, settings.DATABASE.get_async_engine())
 
-    init_app(app, engine)
+
+async def setup_predefined():
+    user_repository = Dependencies.get_user_repository()
+    if not await user_repository.read_by_login(settings.PREDEFINED.FIRST_SUPERUSER_LOGIN):
+        await user_repository.create_superuser(
+            login=settings.PREDEFINED.FIRST_SUPERUSER_LOGIN,
+            password=settings.PREDEFINED.FIRST_SUPERUSER_PASSWORD,
+        )
