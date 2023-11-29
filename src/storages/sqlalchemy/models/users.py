05b7c769ee1d412cb00444f1ae734d0d0d703df2
Ruslan Bel'kov <ruslan.belckov@yandex.ru>
2023-11-29 19:09:59 +0300

    chore: add user role into model

--- src/storages/sqlalchemy/models/users.py
+++ src/storages/sqlalchemy/models/users.py
@@ -1,14 +1,25 @@
 __all__ = ["User"]
 
+from enum import StrEnum
+
+from sqlalchemy import Enum
+
 from src.storages.sqlalchemy.models.__mixin__ import IdMixin
 from src.storages.sqlalchemy.models.base import Base
 from src.storages.sqlalchemy.utils import *
+
+
+class UserRole(StrEnum):
+    DEFAULT = "default"
+    ADMIN = "admin"
 
 
 class User(Base, IdMixin):
     __tablename__ = "users"
 
     name: Mapped[str] = mapped_column(nullable=True)
-    email: Mapped[str] = mapped_column(unique=True)
+    login: Mapped[str] = mapped_column(unique=True)
 
     password_hash: Mapped[str] = mapped_column(nullable=False)
+
+    role: Mapped[UserRole] = mapped_column(Enum(UserRole), nullable=False, default=UserRole.DEFAULT)
