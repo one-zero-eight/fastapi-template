2a4b0dc1116fbc6140cb8b66f0c9e882d08649dc
Ruslan Bel'kov <ruslan.belckov@yandex.ru>
2023-12-02 19:06:35 +0300

    chore: format config.py

--- src/config.py
+++ src/config.py
@@ -3,7 +3,5 @@ from pathlib import Path
 
 from src.config_schema import Settings
 
-settings_path = os.getenv("SETTINGS_PATH")
-if settings_path is None:
-    settings_path = "settings.yaml"
+settings_path = os.getenv("SETTINGS_PATH", "settings.yaml")
 settings: Settings = Settings.from_yaml(Path(settings_path))
