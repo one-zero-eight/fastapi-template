8d97acfb3f1bcc9dd3145f6dd72f28cca9fcc127
Ruslan Bel'kov <ruslan.belckov@yandex.ru>
2025-01-21 23:19:24 +0300

    chore: use 'user' instead of 'users'

--- {{ cookiecutter.project_slug }}/src/storages/{{'mongo'|stencil_path('mongo')}}/__init__.py
+++ {{ cookiecutter.project_slug }}/src/storages/{{'mongo'|stencil_path('mongo')}}/__init__.py
@@ -2,7 +2,7 @@ from typing import cast
 
 from beanie import Document, View
 
-from src.storages.mongo.users import User
+from src.storages.mongo.user import User
 
 document_models = cast(
     list[type[Document] | type[View] | str],
--- {{ cookiecutter.project_slug }}/src/storages/{{'sql'|stencil_path('sql')}}/models/__init__.py
+++ {{ cookiecutter.project_slug }}/src/storages/{{'sql'|stencil_path('sql')}}/models/__init__.py
@@ -1,6 +1,6 @@
 from src.storages.sql.models.base import Base
 
 # Add all models here
-from src.storages.sql.models.users import User
+from src.storages.sql.models.user import User
 
 __all__ = ["Base", "User"]
